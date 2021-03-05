a6f91542db5fa5abe0a9946107f6ab8a5bd57fba
Saïd Redouane <s_red009@hotmail.com>
2021-03-05 02:51:13 +0000

    Add the addProperty function & connect it to the DB

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -88,7 +88,7 @@ exports.getAllReservations = getAllReservations;
 const getAllProperties = function(options, limit = 10) {
   let queryString = `SELECT properties.*, avg(property_reviews.rating) as average_rating
    FROM properties
-   LEFT JOIN property_reviews ON properties.id = property_id`;
+   LEFT JOIN property_reviews ON properties.id = property_id`; //Could of added WHERE TRUE
 
   const queryParams = [];
   const whereWord = " WHERE ";
@@ -140,8 +140,6 @@ const getAllProperties = function(options, limit = 10) {
     LIMIT $${queryParams.length};
     `;
 
-  console.log(queryString, queryParams);
-
   return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 exports.getAllProperties = getAllProperties;
@@ -152,9 +150,28 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+  `;
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+  ];
+  console.log(queryString, queryParams);
+  return pool.query(queryString, queryParams);
 };
 exports.addProperty = addProperty;
